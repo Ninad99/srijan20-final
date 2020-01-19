0815ded1cbd4cd7f6932790d71e0a37291f539eb
ninad99 <ninuch7@gmail.com>
2020-01-19 23:33:29 +0530

    Layout styles updated

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, Switch as RouteSwitch, Route } from 'react-router-dom';
 import { Layout, Dropdown, Menu, Drawer, Icon, Button } from 'antd';
 import { routes } from '../routes';
+import Particles from 'react-particles-js';
 import './AppLayout.css';
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -21,14 +22,50 @@ const AppLayout = props => {
   );
 
   return (
-    <Layout style={{ maxHeight: '100vh' }}>
-    <Drawer
-      bodyStyle={{ padding: 0 }}
-      title="Srijan 20"
-      placement="left"
-      closable={true}
-      onClose={onDrawerClose}
-      visible={drawerVisible}>
+    <Layout className="main-layout">
+      <Particles className="particles"
+        params={{
+          "particles": {
+            "number": {
+                "value": 300,
+                "density": {
+                    "enable": false
+                }
+            },
+            "size": {
+                "value": 5,
+                "random": true
+            },
+            "move": {
+                "direction": "bottom",
+                "out_mode": "out"
+            },
+            "line_linked": {
+                "enable": false
+            }
+          },
+          "interactivity": {
+            "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+                }
+            },
+            "modes": {
+                "repulse": {
+                    "particles_nb": 10
+                }
+            }
+          }
+        }}
+        style={{ height: '100vh', width: '100%' }} />
+      <Drawer
+        bodyStyle={{ padding: 0, zIndex: 100 }}
+        title="Srijan 20"
+        placement="left"
+        closable={true}
+        onClose={onDrawerClose}
+        visible={drawerVisible}>
         <Menu mode="inline">
           {routes.map((route, index) => {
             return (
@@ -41,19 +78,19 @@ const AppLayout = props => {
           })}
         </Menu>
       </Drawer>
-      <Header className="navigation" style={{ height: '8vh' }} theme="dark">
+      <Header className="navigation" theme="dark">
         <Icon type="menu" className="appdrawer-icon" onClick={e => setDrawerVisible(true)} />
         <span className="nav-brand">Srijan 20</span>
         <span className="nav-right">
           <Button style={{ marginRight: '20px' }}>
-          <Icon type="shopping-cart" /> 
+            <Icon type="shopping-cart" /> 
           </Button>
           <Dropdown.Button overlay={dropdownMenu}>
-          <Icon type="user" /> ({username})
+            <Icon type="user" /> ({username})
           </Dropdown.Button>
-      </span>
+        </span>
       </Header>
-      <Layout>
+      <Layout style={{ background: 'transparent' }}>
         <Sider className="dashboard-sider">
           <Menu theme="dark" mode="inline" className="dashboard-sider-menu">
             {routes.map((route, index) => {
@@ -67,14 +104,14 @@ const AppLayout = props => {
             })}
           </Menu>
         </Sider>
-        <Layout style={{ overflowY: 'auto' }}>
+        <Layout style={{ overflowY: 'auto', background: 'transparent' }}>
           <Content>
             <RouteSwitch>
               {routes.map((route, index) => {
                 return <Route key={index} path={route.layout + route.path} component={route.component} />
               })}
             </RouteSwitch>
-            <Footer style={{ textAlign: 'center' }}><strong>Srijan 20</strong> Made with &#9829; by the Faculty of Engineering And Technology Students' Union, <br/> Jadavpur University <br/>Salt Lake Campus Plot No.8, Salt Lake Bypass, LB Block, Sector III, Salt Lake City, Kolkata 700106. </Footer>
+            <Footer style={{ textAlign: 'center', background: 'rgba(22, 104, 159, 0.2)', color: '#00ebff' }}><strong>Srijan 20</strong> Made with &#9829; by the Faculty of Engineering And Technology Students' Union, <br/> Jadavpur University <br/>Salt Lake Campus Plot No.8, Salt Lake Bypass, LB Block, Sector III, Salt Lake City, Kolkata 700106. </Footer>
           </Content>
         </Layout>
       </Layout>
