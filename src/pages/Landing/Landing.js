12c7afe5f3035d534fb2a6424f7fae2868b4bf02
65-121-117-115-104 <ayush_sinha_@outlook.com>
2020-01-19 20:54:54 +0530

    Events Landing and Talks

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Button } from 'antd';
+import fgimg from '../../assets/Images/fg-landing.png';
 import './Landing.css';
 
 const { Title } = Typography;
 
 const Landing = props => {
   const { isAuthenticated } = props;
+  let fgImage;
+  useEffect(() => {
+    
+  }, [])
 
   return (
     <section className="landing">
-      <Title style={{ color: '#fafafa' }}>Srijan 20</Title>
-      <div>
+      <img id="fg-img" className="fgimg" src={ fgimg } alt="landing-fg" />
+      <Title style={{ color: '#fafafa', zIndex: 200 }}>Srijan 20</Title>
+      <div style={{ zIndex: 201 }}>
         {isAuthenticated ? (
           <Link to="/app">
             <Button>Go to Dashboard</Button>
