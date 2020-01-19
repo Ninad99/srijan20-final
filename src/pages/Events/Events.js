78dcbb47d1b328d9607a598fa225c368e1a61b53
65-121-117-115-104 <ayush_sinha_@outlook.com>
2020-01-19 18:57:56 +0530

    Events page half done

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Row, Col, Card } from 'antd';
+import Chart1 from './../../components/Chart1/Chart';
 
 const Events = props => {
   return (
-    <h1>Events</h1>
+    <Row>
+     <Col lg={12} className="flex-container" style={{ padding: '.5rem', textAlign: 'center' }}>
+     <Card title="Your Participation in Srijan 2020 Events" style={{ width: '100%' }}>
+     <Chart1></Chart1>
+     </Card>
+     </Col>
+     <Col lg={12} className="flex-container" style={{ padding: '.5rem', textAlign: 'center' }}>
+     <Card title="Your Participation in Srijan 2020" style={{ width: '100%' }}>
+
+     </Card>
+     </Col>
+    </Row>
   );
 }
 
