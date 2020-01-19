69b23ced266d1cc64eb188344e57d9a5fd190835
ninad99 <ninuch7@gmail.com>
2020-01-19 18:54:55 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React, { useContext, useEffect, useState, useCallback } from 'react';
 import { Switch, Route, withRouter, } from 'react-router-dom';
 import './App.css';
-import firebase, { db } from './firebase/config';
+import firebase from './firebase/config';
 import { AuthContext } from './context/authContext';
-import PrivateRoute from './hoc/PrivateRoute';
+import { getUsernameFromDatabase } from './firebase/utility';
 
+import PrivateRoute from './hoc/PrivateRoute';
 import LandingPage from './pages/Landing/Landing';
 import AppLayout from './layout/AppLayout';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
-
-const getUsernameFromDatabase = async (userId) => {
-  try {
-    const userRef = await db.ref('users/' + userId).once('value');
-    const user = userRef.val();
-    return user.username;
-  } catch (err) {
-    console.log(err);
-    return '';
-  }
-}
-
-const writeUserData = (userId, userName, email, year, department) => {
-  db.ref('users/' + userId).set({
-    username: userName,
-    email: email,
-    year: year,
-    department: department
-  });
-}
 
 const App = props => {
   const { history } = props;
@@ -68,7 +49,7 @@ const App = props => {
     <Switch>
       <Route path='/' exact render={props => <LandingPage isAuthenticated={isAuthenticated} />} />
       <Route path='/login' exact render={props => <Login {...props} />} />
-      <Route path='/register' exact render={props => <Register {...props} writeUserData={writeUserData} />} />
+      <Route path='/register' exact render={props => <Register {...props} />} />
       <PrivateRoute component={AppLayout} path='/app' username={userName} handleSignOut={handleSignOut} />
     </Switch>
   );
