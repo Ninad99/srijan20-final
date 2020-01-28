a0ddebdae0d07e6c3515311acd38d5f3f255e8da
ninad99 <ninuch7@gmail.com>
2020-01-29 02:44:45 +0530

    Merchandise page added

--- src/pages/Merchandise/Merchandise.js
+++ src/pages/Merchandise/Merchandise.js
@@ -1,85 +1,37 @@
-import React from 'react';
-import { Row, Col, Card, Button } from 'antd';
-import Tshirt from './9-2-t-shirt-png-pic-thumb.png'
-import Bag from './kisspng-paper-bag-kraft-paper-corrugated-box-design-shopping-bag-5ab5bfdaa863e8.0828289715218605706897.jpg'
+import React, { useState } from 'react';
+import { Row, Col, Card, Icon } from 'antd';
+import './Merchandise.css';
 
 const Merchandise = () => {
-  return (
-    
-    <section className="dashboard">
-    <Row >
-      <Col md={24} className="flex-container" style={{ padding: '.5rem', textAlign: 'center' }}>
-        <Card title="Buy the official Srijan Clothing Merchandise" style={{ width: '100%' }}>
-          <ul className="custom-carousel">
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Tshirt} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Tshirt} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Tshirt} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Tshirt} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-          </ul>
-        </Card>
-      </Col>
-      </Row>
-      <Row >
-      <Col md={24} className="flex-container" style={{ padding: '.5rem', textAlign: 'center' }}>
-        <Card title="Buy the official Srijan Clothing Merchandise" style={{ width: '100%' }}>
-          <ul className="custom-carousel">
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Bag} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Bag} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Bag} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-            <li className="custom-carousel-item">
-              <Card size="small" title="Event title" style={{ width: 200 }}>
-                <img src={Bag} style={{ marginBottom: '20px',  width: '100%' }} alt="Product img"></img>
-                <Button style={{ marginRight: '20px' }}>+</Button>
-                <Button>-</Button>
-              </Card>
-            </li>
-          </ul>
-        </Card>
-      </Col>
-      </Row>
+  const [count, setCount] = useState(0);
 
+  return (
+  <section className="merchandise">
+    <Row>
+      <Col md={24} className="flex-container" style={{ padding: '.5rem', textAlign: 'center' }}>
+        <Card headStyle={{backgroundColor: 'rgba(22, 104, 159, 0.3)', borderBottom: '2px solid #00ebff', color: '#00ebff' }}
+              bodyStyle={{backgroundColor: 'rgba(22, 104, 159, 0.2)', border: 'none' }}
+              style={{ width: '100%',backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}
+              title="Buy the official Srijan 20 clothing merchandise">
+          <Row>
+            <Col lg={8}>
+              <div className="merchandise-img-container">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftrimamuth.com%2Fwp-content%2Fuploads%2F2016%2F06%2FMammoth-evolution-Hanging-tee.png&f=1&nofb=1" alt="merchandise-img" />
+              </div>
+            </Col>
+            <Col lg={16} className="merchandise-info">
+              <p style={{ color: '#00ebff' }}>blah blah blah</p>
+              <div className="merchandise-actions">
+                <Icon type="plus" className="add-icon" onClick={e => setCount(prevCount => prevCount + 1)} /> <h1>{count}</h1> <Icon type="minus" className="minus-icon" onClick={e => setCount(prevCount => prevCount - 1)} />
+              </div>
+              <span className="btn">
+                <p style={{ padding: 0, margin: 0 }}>Buy Now</p>
+              </span>
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+    </Row>
   </section>
   );
 }
