373353ce2d8286f5962ad50485e64a85eb720db3
ninad99 <ninuch7@gmail.com>
2020-02-06 19:37:50 +0530

    Re-did the events page using css grid

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getEvents } from '../../firebase/utility';
 import { Row, Col, Card, Icon, Spin } from 'antd';
-const { Meta } = Card;
+import './Events.css';
 
 const Events = props => {
   const [fetchedEvents, setFetchedEvents] = useState([]);
@@ -19,35 +19,30 @@ const Events = props => {
     <section>
       <Row>
         {fetchedEvents.length > 0 ? (
-          <Col lg={12} className="flex-container" style={{ padding: '.5rem', textAlign: 'center'}}>
+          <Col lg={24} className="flex-container" style={{ padding: '.5rem', textAlign: 'center'}}>
             <Card headStyle={{backgroundColor: 'rgba(22, 104, 159, 0.3)', borderBottom: '2px solid #00ebff', color: '#00ebff' }}
                   bodyStyle={{backgroundColor: 'rgba(22, 104, 159, 0.2)', border: 'none' }}
-                  style={{ width: '100%',backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}
-                  title="Code"> 
-              <ul className="custom-carousel">
-                {fetchedEvents.map((ev, index) => {
-                  return (
-                    <li className="custom-carousel-item" key={index}>
-                      <Card headStyle={{backgroundColor: 'rgba(22, 104, 159, 0.3)', borderBottom: '2px solid #00ebff', color: '#00ebff' }}
-                            bodyStyle={{ backgroundColor: 'rgba(22, 104, 159, 0.2)', border: 'none', color:'#00ebff' }}
-                            style={{ width: '200px',backgroundColor: 'rgba(0,0,0,0)', border: 'none', color:'#00ebff' }}  
-                            size="small" title={ev.eventName}
-                            cover={
-                              <img
-                                alt="example"
-                                src={"https://robohash.org/" + ev.id}
-                                style={{ width: '100%', marginRight: 'auto'}}
-                              />
-                            }
-                            actions={[
-                              <Link to={"/app/events/" + ev.id} style={{ color: '#00ebff' }}><Icon type="export" />&nbsp;Visit page</Link>
-                            ]}>
-                        <Meta description={<span style={{ color: "#00ebff" }}>This is the description</span>}/>
-                      </Card>
-                    </li>
-                  );
-                })}
-              </ul>
+                  style={{ width: '100%', backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}
+                  title="Srijan Events">
+              <div className="events-grid">
+                {fetchedEvents.map((ev, index) => (
+                  <div className="events-card" key={index}>
+                    <div className="events-card-header">
+                      <div style={{ width: '100%' }}>
+                        <img
+                          alt={ev.eventName + " poster"}
+                          src={ev.photoURL ? ev.photoURL : "https://robohash.org/" + ev.id}
+                          style={{ width: '100%' }}/>
+                      </div>
+                    </div>
+                    <div className="events-card-content">
+                      <strong style={{ color: '#00ebff', fontSize: '1.2rem' }}>{ev.eventName}</strong>
+                      <small style={{ color: '#00ebff', textTransform: 'uppercase' }}>({ev.eventType})</small>
+                      <Link to={"/app/events/" + ev.id} className="events-card-visit-btn"><Icon type="export" />&nbsp;Visit page</Link>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </Card>
           </Col>
         ) : <Col span={24} className="flex-container" ><Spin /></Col>}
