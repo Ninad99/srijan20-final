a1bf1070b7977e67b71ff8da9783fe7a90f45f28
ninad99 <ninuch7@gmail.com>
2020-02-08 11:41:51 +0530

    Updated merchandise page

--- src/pages/Merchandise/Merchandise.js
+++ src/pages/Merchandise/Merchandise.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
-import { Row, Col, Card, Icon } from 'antd';
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
+import { Row, Col, Card, Icon, Form, Input, Button, Spin } from 'antd';
+import { getUserInfo } from '../../firebase/utility';
+import srijanTshirtMockup from '../../assets/Images/srijan_tshirt_mockup.png';
 import './Merchandise.css';
 
 const Merchandise = () => {
-  const [count, setCount] = useState(0);
+  const [userInfo, setUserInfo] = useState(null);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    getUserInfo(currentUser.uid)
+      .then(data => setUserInfo(data))
+      .catch(err => console.log(err))
+  }, [currentUser.uid])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { username, department, college, year, email, printName } = event.target.elements;
+    const data = {
+      name: username.value,
+      department: department.value,
+      college: college.value,
+      year: year.value,
+      email: email.value,
+      printName: printName.value
+    }
+    console.log(data);
+  }
 
   return (
   <section className="merchandise">
@@ -12,21 +36,70 @@ const Merchandise = () => {
         <Card headStyle={{backgroundColor: 'rgba(22, 104, 159, 0.3)', borderBottom: '2px solid #00ebff', color: '#00ebff' }}
               bodyStyle={{backgroundColor: 'rgba(22, 104, 159, 0.2)', border: 'none' }}
               style={{ width: '100%',backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}
-              title="Buy the official Srijan 20 clothing merchandise">
+              title="Buy the official Srijan 20 merchandise">
           <Row>
             <Col lg={8}>
               <div className="merchandise-img-container">
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftrimamuth.com%2Fwp-content%2Fuploads%2F2016%2F06%2FMammoth-evolution-Hanging-tee.png&f=1&nofb=1" alt="merchandise-img" />
+                <img src={srijanTshirtMockup} alt="merchandise-img" />
               </div>
             </Col>
             <Col lg={16} className="merchandise-info">
-              <p style={{ color: '#00ebff' }}>blah blah blah</p>
-              <div className="merchandise-actions">
-                <Icon type="plus" className="add-icon" onClick={e => setCount(prevCount => prevCount + 1)} /> <h1>{count}</h1> <Icon type="minus" className="minus-icon" onClick={e => setCount(prevCount => prevCount - 1)} />
-              </div>
-              <span className="btn">
-                <p style={{ padding: 0, margin: 0 }}>Buy Now</p>
-              </span>
+              {userInfo ? (
+                <Form onSubmit={handleSubmit} layout="horizontal" className="merchandise-form">
+                  <Form.Item label="Name" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="username"
+                      disabled
+                      value={userInfo.username}
+                      prefix={<Icon type="user" style={{ color: '#00ebff' }} />}
+                      placeholder="Name" />
+                  </Form.Item>
+                  <Form.Item label="Department" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="department"
+                      prefix={<Icon type="solution" style={{ color: '#00ebff' }} />}
+                      placeholder="Department" />
+                  </Form.Item>
+                  <Form.Item label="College" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="college"
+                      prefix={<Icon type="book" style={{ color: '#00ebff' }} />}
+                      placeholder="College" />
+                  </Form.Item>
+                  <Form.Item label="Year Of Study" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="year"
+                      type="number"
+                      min={1} max={4}
+                      prefix={<Icon type="read" style={{ color: '#00ebff' }} />}
+                      placeholder="Year Of Study" />
+                  </Form.Item>
+                  <Form.Item label="Email" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="email"
+                      type="email"
+                      disabled
+                      value={userInfo.email}
+                      prefix={<Icon type="mail" style={{ color: '#00ebff' }} />}
+                      placeholder="Email" />
+                  </Form.Item>
+                  <Form.Item label="Print name" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="printName"
+                      prefix={<Icon type="user" style={{ color: '#00ebff' }} />}
+                      placeholder="Name to be printed on T-shirt" />
+                  </Form.Item>
+                  <Button htmlType="submit" className="merchandise-submit-btn" style={{ color: '#00ebff' }}>
+                    Proceed to checkout
+                  </Button>
+                </Form>
+              ) : <Spin />}
             </Col>
           </Row>
         </Card>
