0fd1149cecbe013c247991502fba9b7a9034fc09
ninad99 <ninuch7@gmail.com>
2020-02-11 03:07:19 +0530

    Chart renders data from props

--- src/components/DoughnutChart/DoughnutChart.js
+++ src/components/DoughnutChart/DoughnutChart.js
@@ -2,37 +2,30 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const data = {
-  datasets: [{
-    data: [3, 5, 2, 1, 5],
-    backgroundColor: [
-      '#29b6f6',
-      '#00e676',
-      '#ffee58',
-      '#1a237e',
-      '#e91e63',
-      '#ab47bc'
-    ],
-    borderColor: [
-      '#29b6f6',
-      '#00e676',
-      '#ffee58',
-      '#1a237e',
-      '#e91e63',
-      '#ab47bc'
-    ]
-  }],
-  labels: [
-    'Coding',
-    'Robotics',
-    'Quiz',
-    'Gaming',
-    'Misc'
-  ],
-};
+const DoughnutChart = props => {
+  const data = {
+    datasets: [{
+      data: props.data,
+      backgroundColor: [
+        '#29b6f6',
+        '#00e676',
+        '#ffee58',
+        '#1a237e',
+        '#e91e63',
+        '#ab47bc'
+      ],
+      borderColor: [
+        '#29b6f6',
+        '#00e676',
+        '#ffee58',
+        '#1a237e',
+        '#e91e63',
+        '#ab47bc'
+      ]
+    }],
+    labels: props.labels,
+  };
 
-
-const DoughnutChart = () => {
 
   return (  
     <Doughnut data={data} legend={{
