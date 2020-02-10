41a017f28bf8773c2037c504ec914dddd28ef8d0
ninad99 <ninuch7@gmail.com>
2020-02-11 01:33:02 +0530

    Added merchandise form + checkout

--- src/pages/Merchandise/Merchandise.js
+++ src/pages/Merchandise/Merchandise.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../../context/authContext';
-import { Row, Col, Card, Icon, Form, Input, Button, Spin } from 'antd';
+import { Row, Col, Card, Icon, Form, Input, Button, Spin, Alert } from 'antd';
 import { getUserInfo } from '../../firebase/utility';
+import { firestore } from '../../firebase/config';
 import srijanTshirtMockup from '../../assets/Images/srijan_tshirt_mockup.png';
 import './Merchandise.css';
 
+const isValid = (department, college, year, printName, phoneNo) => {
+  return (department !== "") && (college !== "") && (year !== "") && (printName !== "") && (phoneNo !== "");
+}
+
 const Merchandise = () => {
   const [userInfo, setUserInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formError, setFormError] = useState(false);
   const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
@@ -17,16 +24,33 @@ const Merchandise = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const { username, department, college, year, email, printName } = event.target.elements;
-    const data = {
-      name: username.value,
-      department: department.value,
-      college: college.value,
-      year: year.value,
-      email: email.value,
-      printName: printName.value
+    setIsLoading(true);
+    const { username, department, college, year, email, printName, phoneNumber } = event.target.elements;
+    if (isValid(department.value, college.value, year.value, printName.value, phoneNumber.value)) {
+      const date = new Date();
+      const data = {
+        date: date.toDateString() + " " + date.toTimeString(),
+        name: username.value,
+        department: department.value,
+        college: college.value,
+        year: year.value,
+        email: email.value,
+        printName: printName.value,
+        phoneNo: phoneNumber.value,
+        paymentStatus: "pending",
+        userId: currentUser.uid,
+        amount: 300
+      }
+      firestore.collection('orders').add(data)
+        .then(docRef => {
+          window.location.href = `http://localhost:2000/merch/txn?orderId=${docRef.id}&amount=300`;
+        })
+        .catch(err => console.log(err));
+      console.log(data);
+    } else {
+      setFormError(true);
+      setIsLoading(false);
     }
-    console.log(data);
   }
 
   return (
@@ -38,14 +62,16 @@ const Merchandise = () => {
               style={{ width: '100%',backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}
               title="Buy the official Srijan 20 merchandise">
           <Row>
-            <Col lg={8}>
+            <Col lg={10}>
               <div className="merchandise-img-container">
                 <img src={srijanTshirtMockup} alt="merchandise-img" />
               </div>
             </Col>
-            <Col lg={16} className="merchandise-info">
+            <Col lg={14} className="merchandise-info">
               {userInfo ? (
                 <Form onSubmit={handleSubmit} layout="horizontal" className="merchandise-form">
+                  {formError ? <Alert message="Form fields can't be blank!" type="error" /> : null}
+                  <br />
                   <Form.Item label="Name" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
                     <Input
                       className="merchandise-input"
@@ -95,8 +121,16 @@ const Merchandise = () => {
                       prefix={<Icon type="user" style={{ color: '#00ebff' }} />}
                       placeholder="Name to be printed on T-shirt" />
                   </Form.Item>
+                  <Form.Item label="Phone no." labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+                    <Input
+                      className="merchandise-input"
+                      name="phoneNumber"
+                      type="number"
+                      prefix={<Icon type="phone" style={{ color: '#00ebff' }} />}
+                      placeholder="Phone no." />
+                  </Form.Item>
                   <Button htmlType="submit" className="merchandise-submit-btn" style={{ color: '#00ebff' }}>
-                    Proceed to checkout
+                    Proceed to checkout&nbsp;&nbsp;{isLoading ? <Spin /> : null}
                   </Button>
                 </Form>
               ) : <Spin />}
