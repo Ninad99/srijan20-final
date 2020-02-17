f60a5e7564c95a3f7c856ce5940f03ffa876d0f0
ninad99 <ninuch7@gmail.com>
2020-02-17 13:30:30 +0530

    Storing last location url

--- src/hoc/PrivateRoute.js
+++ src/hoc/PrivateRoute.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { AuthContext } from '../context/authContext';
+import { LastLocationContext } from '../context/lastLocationContext';
 
 const PrivateRoute = ({component: Component, path: Path, ...rest}) => {
   const { currentUser } = useContext(AuthContext);
-  const { location, setLastLocation } = rest;
+  const { location } = rest;
+  const { setLastLocation } = useContext(LastLocationContext);
 
   if (!currentUser && location.pathname.includes('/app/')) {
     setLastLocation(location.pathname);
