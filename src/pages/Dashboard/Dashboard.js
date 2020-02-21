2cfb8b37f08ad8a49e4f2e77d1b2e8e9c9f0bf1f
ninad99 <ninuch7@gmail.com>
2020-02-21 21:58:00 +0530

    Fetch user events

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../../context/authContext';
-import { getUserInfo, getUserEvents } from '../../firebase/utility';
+import { getUserEvents } from '../../firebase/utility';
 import { Row, Col, Card, Spin } from 'antd';
 import DoughnutChart from '../../components/DoughnutChart/DoughnutChart';
 import SrijanAppImage from '../../assets/Images/app-min.png';
@@ -10,19 +10,21 @@ const { Meta } = Card;
 
 const Dashboard = props => {
   const { currentUser } = useContext(AuthContext);
-  const [userInfo, setUserInfo] = useState(null);
-  const [userEvents, setUserEvents] = useState([]);
+  // const [userInfo, setUserInfo] = useState(null);
+  const [userEvents, setUserEvents] = useState(null);
 
   useEffect(() => {
-    getUserInfo(currentUser.uid)
-      .then(async userDetails => {
-        setUserInfo(userDetails)
-      })
-      .catch(err => console.log(err));
+    // getUserInfo(currentUser.uid)
+    //   .then(async userDetails => {
+    //     console.log(userDetails);
+    //     setUserInfo(userDetails)
+    //   })
+    //   .catch(err => console.log(err));
     
-      getUserEvents(currentUser.uid)
+    getUserEvents(currentUser.uid)
       .then(async userEvents => {
         const events = [];
+        console.log(userEvents);
         for (const eventId in userEvents) {
           events.push(userEvents[eventId]);
         }
@@ -35,7 +37,7 @@ const Dashboard = props => {
     <section className="dashboard">
       <Row>
         <Col md={24} className="flex-container" style={{ padding: '.5rem', textAlign: 'center'}}>
-        {userInfo ? (
+        {userEvents ? (
           <Card headStyle={{backgroundColor: 'rgba(22, 104, 159, 0.3)', borderBottom: '2px solid #00ebff', color: '#00ebff' }}
                 bodyStyle={{backgroundColor: 'rgba(22, 104, 159, 0.2)', border: 'none' }}
                 style={{ width: '100%',backgroundColor: 'rgba(0,0,0,0)', border: 'none' }}
@@ -51,15 +53,8 @@ const Dashboard = props => {
                   <Card headStyle={{backgroundColor: 'rgba(22, 104, 159, 0.3)', borderBottom: '2px solid #00ebff', color: '#00ebff' }}
                         bodyStyle={{ backgroundColor: 'rgba(22, 104, 159, 0.2)', border: 'none', color:'#00ebff' }}
                         style={{ width: '200px',backgroundColor: 'rgba(0,0,0,0)', border: 'none', color:'#00ebff' }}  
-                        size="small" title={ev.event}
-                        cover={
-                          <div style={{ width: '100%', padding: 0 }}>
-                            <img
-                              alt="example"
-                              src={ev.poster ? ev.poster : "https://robohash.org/" + ev.code}
-                              style={{ width: '100%', height: '40%' }}/>
-                          </div>
-                        }>
+                        size="small"
+                        title={ev.event}>
                     <Meta
                         title={<span style={{ color: "#00ebff", textTransform: 'uppercase' }}>Team: {ev.team}</span>}/>
                   </Card>
